1cecf133afb04e5b4451dc255efe43945193b29f
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2025-03-07 21:40:33 +0100

    Worked on the project responsiveness

--- public/assets/icons/index.js
+++ public/assets/icons/index.js
@@ -2,3 +2,6 @@ export { default as bulbIcon } from "./bulbIcon.svg";
 export { default as clockIcon } from "./clockIcon.svg";
 export { default as fileIcon } from "./fileIcon.svg";
 export { default as penIcon } from "./penIcon.svg";
+export { default as phoneIcon } from "./phoneIcon.svg";
+export { default as chatIcon } from "./chatIcon.svg";
+export { default as envelopeIcon } from "./envelopeIcon.svg";
